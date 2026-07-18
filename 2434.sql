fbf56c04430c02897c8044c5017a74b5e4f8a982
Khoa Tran <ktran4618@gmail.com>
2026-07-18 09:53:01 -0400

    Delete 2434.sql

--- 2434.sql
+++
@@ -1,7 +0,0 @@
-/*SELECT * FROM raw.ingest_log ORDER BY started_at DESC;
-*/
-
-SELECT table_name, column_name, ordinal_position
-FROM information_schema.columns
-WHERE table_schema = 'raw'
-ORDER BY table_name, ordinal_position;
