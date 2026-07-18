0ab7852346cee89c2867abb6723bc2a4380d7f76
Khoa Tran <ktran4618@gmail.com>
2026-07-18 09:53:24 -0400

    Delete 35677.sql

--- 35677.sql
+++
@@ -1,6 +0,0 @@
-SELECT
-  COUNT(*) AS total,
-  COUNT(*) FILTER (WHERE order_id IS NULL OR order_id = '') AS blank_order_id,
-  COUNT(*) FILTER (WHERE order_date IS NULL OR order_date = '') AS blank_order_date,
-  COUNT(DISTINCT order_id) FILTER (WHERE order_date IS NOT NULL AND order_date <> '') AS distinct_id_with_valid_date
-FROM raw.sales_transactions;
